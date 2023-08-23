e151d4134b023954e4df2fe3b880e4209310f599
Pheobhe <l.defalco@mjus.gba.gob.ar>
2023-08-23 12:03:50 -0300

    Vue router

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,30 @@
 <template>
- <!-- <h1>Practica Componentes dinámicos</h1> -->
- <div class="header">
-  <h3 class="header__lista">Vue Cursos</h3>
-    <ul class="header__lista">
-        <li class="header__lista__item">
-          <a 
-            @click="mostrarComponente('lista-cursos', 'Lista de Cursos')"
-            class="header__lista__item__link">
-            Cursos
-        </a>
-        </li>
-        <li class="header__lista__item">
-          <a 
-          @click="mostrarComponente('lista-profesores', 'Lista de Profesores')"
-            class="header__lista__item__link"> 
-            Profesores
-          </a>
-        </li>
-        <li class="header__lista__item">
-          <a 
-          @click="mostrarComponente('contactos-cursos', 'Formulario de Contacto')"
-            class="header__lista__item__link"> 
-            Contactos
-          </a>
-        </li>
-    </ul>
- </div>
-
- <keep-alive>   <!-- almacena en cache la info cuando navego de un item a otro -->
-    <component 
-      :is="componentSelect"
-      :titulo="propComponent"
-      />
- </keep-alive>
-
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-
-import ListaCursos from '@/components/cursos/ListaCursos'
-import ListaProfesores from '@/components/cursos/ListaProfesores'
-import ContactosCursos from '@/components/cursos/ContactosCursos'
-export default {
-  name: 'App',
-  data() {
-    return {
-      componentSelect: null,
-      propComponent: ''
-     }
-   },
-  components: {
-    ListaCursos,
-    ListaProfesores,
-    ContactosCursos
-  },
-  methods: {
-      mostrarComponente: function (componente, propiedad){
-       this.componentSelect = componente
-       this.propComponent = propiedad
-      }
-    }  
-}
-</script>
-
 <style lang="scss">
-
-*{
-  margin: 0px;
-  padding: 0px;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-.header {
-  padding: 1em;
-  border: 1px solid;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  &__lista{
-     display: flex;
-     list-style: none;
-     padding: 0;
-     margin: 0;
-     &__item{
-      padding: 1em;
 
-     }
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
   }
 }
 </style>
